8a527dbcd187f0347ea356b24f392c2050fe778e
Du Chenyang <chenyang.du@intel.com>
2012-05-17 17:57:24 +0800

    ALL_PREBUILT is not allowed to use in component module in ICS. PRODUCT_COPY_FILES is more appriciated.
    BZ: 36629
    
    Change-Id: I54e93eb6eb1e743d96eb1322eb4ad45da983249c
    Signed-off-by:  Sun Yijin <yijin.sun@intel.com>
    Reviewed-on: http://android.intel.com:8080/49211
    Reviewed-by: Du, Chenyang <chenyang.du@intel.com>
    Reviewed-by: Beare, Bruce J <bruce.j.beare@intel.com>
    Reviewed-by: Yin, Fengwei <fengwei.yin@intel.com>
    Tested-by: Li, XiaojingX <xiaojingx.li@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- Android.mk
+++ Android.mk
@@ -14,32 +14,24 @@ include $(CLEAR_VARS)
 TARGET_ALSA_CONF_DIR := $(TARGET_OUT)/usr/share/alsa
 LOCAL_ALSA_CONF_DIR  := $(LOCAL_PATH)/src/conf
 
-copy_from := \
-	alsa.conf \
-	pcm/dsnoop.conf \
-	pcm/modem.conf \
-	pcm/dpl.conf \
-	pcm/default.conf \
-	pcm/surround51.conf \
-	pcm/surround41.conf \
-	pcm/surround50.conf \
-	pcm/dmix.conf \
-	pcm/center_lfe.conf \
-	pcm/surround40.conf \
-	pcm/side.conf \
-	pcm/iec958.conf \
-	pcm/rear.conf \
-	pcm/surround71.conf \
-	pcm/front.conf \
-	cards/aliases.conf
-
-copy_to   := $(addprefix $(TARGET_ALSA_CONF_DIR)/,$(copy_from))
-copy_from := $(addprefix $(LOCAL_ALSA_CONF_DIR)/,$(copy_from))
-
-$(copy_to) : $(TARGET_ALSA_CONF_DIR)/% : $(LOCAL_ALSA_CONF_DIR)/% | $(ACP)
-	$(transform-prebuilt-to-target)
-
-ALL_PREBUILT += $(copy_to)
+PRODUCT_COPY_FILES += \
+        $(LOCAL_ALSA_CONF_DIR)/alsa.conf:system/usr/share/alsa/alsa.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/dsnoop.conf:system/usr/share/alsa/pcm/dsnoop.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/modem.conf:system/usr/share/alsa/pcm/modem.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/dpl.conf:system/usr/share/alsa/pcm/dpl.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/default.conf:system/usr/share/alsa/pcm/default.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/surround51.conf:system/usr/share/alsa/pcm/surround51.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/surround41.conf:system/usr/share/alsa/pcm/surround41.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/surround50.conf:system/usr/share/alsa/pcm/surround50.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/dmix.conf:system/usr/share/alsa/pcm/dmix.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/center_lfe.conf:system/usr/share/alsa/pcm/center_lfe.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/surround40.conf:system/usr/share/alsa/pcm/surround40.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/side.conf:system/usr/share/alsa/pcm/side.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/iec958.conf:system/usr/share/alsa/pcm/iec958.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/rear.conf:system/usr/share/alsa/pcm/rear.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/surround71.conf:system/usr/share/alsa/pcm/surround71.conf \
+        $(LOCAL_ALSA_CONF_DIR)/pcm/front.conf:system/usr/share/alsa/pcm/front.conf \
+        $(LOCAL_ALSA_CONF_DIR)/cards/aliases.conf:system/usr/share/alsa/cards/aliases.conf
 
 include $(CLEAR_VARS)
 
